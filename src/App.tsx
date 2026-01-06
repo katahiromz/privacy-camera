ff74d56688ebbb49c15b8247a6e4c357adb4a074
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2026-01-06 10:57:27 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,276 @@
+// App.tsx --- アプリのTypeScriptソース
+// Author: katahiromz
+// License: MIT
+import React, { useRef, useState, useEffect, useCallback } from 'react';
+import CanvasWithWebcam03, { ImageProcessData } from './components/CanvasWithWebcam03';
+import { isAndroidApp, emulateInsets, saveMedia, saveMediaEx, polyfillGetUserMedia } from './libs/utils.ts';
+import './App.css';
+
+const IS_PRODUCTION = import.meta.env.MODE === 'production'; // 製品版か？
+const SHOW_CONFIG = true; // 設定ボタンを表示するか？
+const ENABLE_CONFIG = true; // 設定を有効にするか？
+
+// 国際化(i18n)
+import './libs/i18n.ts';
+import { useTranslation } from 'react-i18next';
+
+// アプリケーションのベースパスを取得
+const BASE_URL = import.meta.env.BASE_URL;
+
+const ENABLE_KEYS = true; // キーボード操作するか？
+const ENABLE_FACE_DETECTION = true; // 顔認識を有効にするか？
+const SHOW_CURRENT_TIME = false; // 現在の日時を表示するか？
+
+// ダミー画像
+const dummyImageUrl = `${BASE_URL}example-qr-code.png`;
+//const dummyImageUrl = `${BASE_URL}dummy.jpg`;
+const USE_DUMMY_IMAGE = false;
+//const USE_DUMMY_IMAGE = true;
+
+// 音声のURL
+const shutterSoundUrl = `${BASE_URL}ac-camera-shutter-sound.mp3`;
+const videoStartSoundUrl = `${BASE_URL}ac-video-started.mp3`;
+const videoCompleteSoundUrl = `${BASE_URL}ac-video-completed.mp3`;
+
+if (!IS_PRODUCTION) { // 本番環境ではない場合、
+  emulateInsets(); // insetsをエミュレートする
+}
+
+// 古いブラウザのサポート(必要か？)
+polyfillGetUserMedia();
+
+// 画像処理関数
+const onImageProcess = async (data: ImageProcessData) => {
+  const { x, y, width, height, src, srcWidth, srcHeight, video, canvas, isMirrored, currentZoom, offset, showCodes, qrResultsRef } = data;
+  const ctx = canvas.getContext('2d',
+    { alpha: false, desynchronized: true, willReadFrequently: false } // 速度優先
+  );
+
+  if (!ctx || width <= 0 || height <= 0) return;
+
+  // 鏡像なら左右反転の座標変換
+  if (isMirrored) {
+    ctx.translate(width, 0);
+    ctx.scale(-1, 1);
+  }
+
+  if (currentZoom !== 1.0 || offset.x != 0 || offset.y != 0) {
+    // 背景を塗りつぶす
+    if (BACKGROUND_IS_WHITE) {
+      ctx.fillStyle = 'white';
+      ctx.fillRect(x, y, width, height);
+    } else {
+      ctx.clearRect(x, y, width, height);
+    }
+
+    // ズーム前のソースのサイズ
+    const sourceWidth = srcWidth / currentZoom;
+    const sourceHeight = srcHeight / currentZoom;
+
+    // Offsetを含めた中央基準の計算
+    const maxOffsetX = (srcWidth - sourceWidth) / 2;
+    const maxOffsetY = (srcHeight - sourceHeight) / 2;
+
+    // ソースの位置
+    const sourceX = maxOffsetX + offset.x;
+    const sourceY = maxOffsetY + offset.y;
+
+    // イメージを拡大縮小して転送
+    ctx.drawImage(
+      src, Math.round(sourceX), Math.round(sourceY), sourceWidth, sourceHeight,
+      x, y, width, height
+    );
+  } else {
+    // ズームなし、パンなし
+    ctx.drawImage(src, x, y, width, height);
+  }
+
+  ctx.setTransform(1, 0, 0, 1, 0, 0); // 座標変換を元に戻す
+
+  let minxy = Math.min(width, height);
+  let maxxy = Math.max(width, height);
+  let avgxy = (width + height) / 2;
+
+  if (ENABLE_FACE_DETECTION) { // 顔認識を有効にするか？
+    // TODO: 顔認識
+  }
+
+  if (SHOW_CURRENT_TIME) { // ちょっと日時を描画してみるか？
+    let text = getLocalDateTimeString();
+    ctx.font = `${minxy * 0.05}px monospace, san-serif`;
+    let measure = ctx.measureText(text);
+    const margin = minxy * 0.015;
+    let x0 = x + width - measure.width - margin, y0 = height - margin;
+    ctx.strokeStyle = "#000";
+    ctx.lineWidth = minxy * 0.01;
+    ctx.strokeText(text, x0, y0);
+    ctx.fillStyle = "#0f0";
+    ctx.fillText(text, x0, y0);
+  }
+};
+
+// アプリ
+function App() {
+  const { t } = useTranslation(); // 翻訳用
+  const canvasWithCamera = useRef<CanvasWithWebcam03>(null);
+
+  // 設定をする
+  const doConfig = () => {
+    if (!ENABLE_CONFIG)
+      return;
+    alert(t('camera_app_info'));
+  };
+
+  useEffect(() => {
+    //console.log(canvasWithCamera.current.canvas);
+    //canvasWithCamera.current.setZoomRatio(2);
+    //console.log(canvasWithCamera.current.getZoomRatio());
+  }, []);
+
+  // 物理の音量ボタンを押されたら撮影
+  useEffect(() => {
+    // ハンドラ関数の定義
+    const handlePhysicalVolumeButton = (e: any) => {
+      // Android側から CustomEvent("PhysicalVolumeButton", { detail: ... }) で送られてくることを想定
+      const { volumeType } = e.detail || {};
+      console.log(`Volume: ${volumeType}`);
+
+      // 音量ボタンでシャッターを切るなど
+      canvasWithCamera.current?.takePhoto();
+    };
+
+    // イベントリスナーの登録
+    window.addEventListener('PhysicalVolumeButton', handlePhysicalVolumeButton, { passive: false });
+
+    // クリーンアップ（コンポーネント消滅時に解除）
+    return () => {
+      window.removeEventListener('PhysicalVolumeButton', handlePhysicalVolumeButton);
+    };
+  }, []); // 初回マウント時のみ実行
+
+  useEffect(() => {
+    // Android側から呼ばれるグローバル関数を定義
+    if ((window as any).onPhysicalVolumeButton) {
+      (window as any).onPhysicalVolumeButton = () => {
+        canvasWithCamera.current?.takePhoto();
+      };
+    }
+    // コンポーネントがアンマウントされる時にクリーンアップ
+    return () => {
+      delete (window as any).onPhysicalVolumeButton;
+    };
+  }, []);
+
+  // キーボード操作を可能にする
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (!ENABLE_KEYS) return;
+      switch(event.key) {
+      case '+': // ズームイン
+      case ';': // (日本語キーボード対応用)
+        if (!event.ctrlKey && !event.altKey) { // CtrlキーやAltキーが押されていない？
+          event.preventDefault();
+          canvasWithCamera.current?.zoomIn(); // ズームイン
+        }
+        break;
+      case '-': // ズームアウト
+        if (!event.ctrlKey && !event.altKey) { // CtrlキーやAltキーが押されていない？
+          event.preventDefault();
+          canvasWithCamera.current?.zoomOut(); // ズームアウト
+        }
+        break;
+      case ' ': // スペース キー
+        if (!event.ctrlKey && !event.altKey) { // CtrlキーやAltキーが押されていない？
+          event.preventDefault();
+          canvasWithCamera.current?.takePhoto(); // 写真撮影
+        }
+        break;
+      case 'Enter': // Enterキー
+        if (!event.ctrlKey && !event.altKey) { // CtrlキーやAltキーが押されていない？
+          event.preventDefault();
+          // 録画開始・録画停止を切り替える
+          if (canvasWithCamera.current?.isRecording()) {
+            canvasWithCamera.current?.stopRecording();
+          } else {
+            canvasWithCamera.current?.startRecording();
+          }
+        }
+        break;
+      // パン操作 (矢印)
+      case 'ArrowUp':
+        event.preventDefault();
+        canvasWithCamera.current?.panUp();
+        break;
+      case 'ArrowDown':
+        event.preventDefault();
+        canvasWithCamera.current?.panDown();
+        break;
+      case 'ArrowLeft':
+        event.preventDefault();
+        canvasWithCamera.current?.panRight();
+        break;
+      case 'ArrowRight':
+        event.preventDefault();
+        canvasWithCamera.current?.panLeft();
+        break;
+      default:
+        //console.log(event.key);
+        break;
+      }
+    };
+
+    document.body.addEventListener('keydown', handleKeyDown);
+    return () => document.body.removeEventListener('keydown', handleKeyDown);
+  }, []);
+
+  // メッセージを処理する
+  useEffect(() => {
+    const onMessage = (e) => {
+      switch (e.data) {
+      case 'go_back': // Android標準の「戻る」ボタンをサポートする。
+        if (window.android) {
+          e.preventDefault(); // イベントのデフォルトの処理をスキップ。
+          // 可能ならばアプリを閉じる(完全に終了する訳ではない)
+          try { window.android.finishApp(); } catch (err) { }
+        }
+        break;
+      case 'onAppResume': // Androidアプリ再開時の処理を行う。
+        if (window.android) {
+          e.preventDefault(); // イベントのデフォルトの処理をスキップ。
+          canvasWithCamera.current?.onAppResume();
+        }
+        break;
+      default:
+        console.log(e.data);
+        break;
+      }
+    };
+
+    window.addEventListener('message', onMessage, { passive: false });
+    return () => {
+      window.removeEventListener('message', onMessage);
+    }
+  }, []);
+
+  return (
+    <CanvasWithWebcam03
+      ref={canvasWithCamera}
+      width="100%"
+      height="100%"
+      shutterSoundUrl={shutterSoundUrl}
+      videoStartSoundUrl={videoStartSoundUrl}
+      videoCompleteSoundUrl={videoCompleteSoundUrl}
+      downloadFile={isAndroidApp ? saveMediaEx : saveMedia}
+      eventTarget={document.body}
+      autoMirror={false}
+      onImageProcess={onImageProcess}
+      dummyImageSrc={ USE_DUMMY_IMAGE ? dummyImageUrl : null }
+      showConfig={SHOW_CONFIG}
+      showCodeReader={false}
+      doConfig={doConfig}
+      aria-label={t('camera_app')}
+    />
+  );
+}
+
+export default App;
